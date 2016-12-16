cd57349ec1a69610f4a3ad81781a66e74235395b
genffy <lee.genffy@gmail.com>
2016-12-16 16:03:35 +0800

    update

--- test/specs/gfs.spec.js
+++ test/specs/gfs.spec.js
@@ -1,5 +1,21 @@
 /**
  * Future normal test
  */
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var _ = require('lodash');
+var rx = require('rx');
+var gfs = require('../../src/init-cli');
+var autosubmit = require('../helpers/events').autosubmit;
+
 describe('gfs', function () {
+    beforeEach(function () {
+        console.log(gfs)
+    })
+
+
+    it( 'should show help list', function () {
+
+    })
+
 });
